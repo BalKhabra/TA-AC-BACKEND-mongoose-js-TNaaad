3677ff4be8c3f1b93597d49d628af711c48275aa
Baljit Khabra <baljitkhabra99@gmail.com>
2022-07-16 13:47:56 -0700

    complete

--- block-BNaacf/models/user.js
+++ block-BNaacf/models/user.js
@@ -2,20 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    village: String,
-    city: String,
-    state: String,
-    pin: Number,
-    user: ObjectId
-
-    
+    namr: String,
+    email: {type: String, lowercase: true},
+    age: Number,
+    books: [String],
+    grades: [Number]
 })
 
-var user = {
-    village: kaharpur,
-    city: mahilpur,
-    state: punjab,
-    pin: 2323,
-    user: khabra
-
-}
